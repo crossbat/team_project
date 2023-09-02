c4a7a343dafa4b5f6829e508e37b5b9213569b32
crossbat <xowls1999@gmail.com>
2023-09-02 11:02:21 +0900

    rewrite

--- post/models.py
+++ post/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+'''class Post(models.Model):
+    subject = models.CharField(max_length = 200)
+    writer = model.CharField(default = "Administer", max_length = 200)
+    content = '''
